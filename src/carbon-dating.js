80e92aecaf3a0ee9d1dab6013e7a07fef0f31f1f
edavetisyan <edmondavetisyanw@gmail.com>
2021-10-05 23:55:43 +0300

    feat: carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,4 +1,4 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from "../extensions/index.js";
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
@@ -6,18 +6,36 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(sampleActivity) {
+  // check if argument string and number
+  let sampleActivityNum = parseFloat(sampleActivity);
+  if (typeof sampleActivity !== "string" || isNaN(sampleActivityNum)) {
+    // console.log(false);
+    return false;
+  }
+  // console.log(sampleActivity);
+  if(sampleActivityNum> MODERN_ACTIVITY || sampleActivityNum <=0){
+    // console.log(false);
+    return false
+  }
+  let logOf2 = 0.693 //approximation of the natural logarithm of two.
+   let ageOfFind = Math.ceil(Math.log(MODERN_ACTIVITY/ sampleActivityNum) / (logOf2/HALF_LIFE_PERIOD))
+  //  console.log(ageOfFind);
+   return ageOfFind
 }
+
+dateSample("1");
+dateSample("Woo");
+dateSample(1);
+dateSample("22387");
